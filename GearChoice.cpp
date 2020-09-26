29f1db4372d68e9e94e517c65eaaf0a25db1592b
imwilson <imwilson@umich.edu>
2020-09-26 19:05:03 -0400

    adding more gear choice prompts

--- GearChoice.cpp
+++ GearChoice.cpp
@@ -5,21 +5,74 @@ using namespace std;
 void ask_helm() {
 	cout << "Enter your helm from the list below: " << endl;
 	cout << "
-	Bonescythe helm \n
-	Deathdealer helm \n
-	Bloodfang helm \n
-	Nightslayer helm \n
-	Darkmantle helm \n
-	Shadowcraft Cap \n
+	1. Bonescythe helm \n
+	2. Deathdealer helm \n
+	3. Bloodfang helm \n
+	4. Nightslayer helm \n
+	5. Darkmantle helm \n
+	6. Shadowcraft Cap \n
 	" << endl;
 
 }
-void ask_neck();
-void ask_shoulder();
-void ask_shirt();
-void ask_chest();
-void ask_legs();
-void ask_feet();
+void ask_neck() {
+	cout << "Enter your neck from the list below: " << endl;
+	cout << "
+	1. Prestor's \n
+	2. Stormrage's \n
+	3. Onyxia \n
+	4. Eye of Hakkar \n
+	5. Mark of Fordring \n
+	" << endl;
+}
+void ask_shoulder() {
+	cout << "Enter your shoulder from the list below: " << endl;
+	cout << "
+	1. Bonescythe Pauldrons \n
+	2. Deathdealer Spaulders \n
+	3. Bloodfang Spaulders \n
+	4. Nightslayer Shoulder Pads \n
+	5. Darkmantle Spaulders \n
+	6. Shadowcraft Spaulders \n
+	7. Zandalar Madcap Spaulders \n
+	" << endl;
+}
+void ask_shirt() {
+	cout << "Why do you care about your shirt, lolwutf?" << endl;
+}
+void ask_chest() {
+	cout << "Enter your chestpiece from the list below: " << endl;
+	cout << "
+	1. Bonescythe Breastplate \n
+	2. Deathdealer Vest \n
+	3. Bloodfang Chestpiece \n
+	4. Nightslayer Chestpiece \n
+	5. Darkmantle Tunic \n
+	6. Shadowcraft Tunic \n
+	7. Zandalar Madcap Tunic \n
+	" << endl;
+}
+void ask_legs() {
+	cout << "Enter your legs from the list below: " << endl;
+	cout << "
+	1. Bonescythe Legplates \n
+	2. Deathdealer Leggings \n
+	3. Bloodfang Pants \n
+	4. Nightslayer Pants \n
+	5. Darkmantle Pants \n
+	6. Shadowcraft Pants \n
+	" << endl;
+}
+void ask_feet() {
+	cout << "Enter your boots from the list below: " << endl;
+	cout << "
+	1. Bonescythe Sabatons \n
+	2. Deathdealer Boots \n
+	3. Bloodfang Boots \n
+	4. Nightslayer Boots \n
+	5. Darkmantle Boots \n
+	6. Shadowcraft Boots \n
+	" << endl;
+}
 void ask_wrist();
 void ask_hands();
 void ask_ring1();
